83ee59d0364356f338ca5e9f5f8028386bb17e8e
Stephen Cook <sitiveni@gmail.com>
2009-05-03 21:05:21 +1200

    make it hopefully portable to 32bit, python is a PITA with ints!

--- boptester.py
+++ boptester.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 
 import subprocess
+import sys
 
 class RandomTest:	
 	def runTest(self):
@@ -19,7 +20,11 @@ class RandomTest:
 			else:
 				randomness = matchedBytes
 		random_bits = 0
-		for x in xrange(0,32):
+		if sys.maxint > (1 << 33):
+			num_bits = 64
+		else:
+			num_bits = 32
+		for x in xrange(0,num_bits):
 			if not(randomness & (1 << x)):
 				random_bits += 1
 		return random_bits
